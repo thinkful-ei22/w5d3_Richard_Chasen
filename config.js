66c01496d1d7f4d0b3bd1029ae649a249dc798dd
ccareyncsu <ccarey@ncsu.edu>
2018-07-11 16:33:57 -0600

    added new key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://user2:ASDF1234@ds231951.mlab.com:31951/my-first-mlab-db';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://dbuser:dbpassword1@ds133621.mlab.com:33621/node-restaurants-app-mongoose';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
